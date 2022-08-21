2d77afd0e22874dee3b8d9f4439b51bd8c102690
Alex Toktosunov <alekstok@amazon.com>
2022-08-21 17:19:29 +0100

    Added NvimTree configs

--- init.lua
+++ init.lua
@@ -6,6 +6,8 @@ Plug 'kyazdani42/nvim-tree.lua'
 Plug ('nvim-treesitter/nvim-treesitter', {['do'] = ':TSUpdate'})
 Plug 'shaunsingh/nord.nvim'
 Plug 'folke/which-key.nvim'
+Plug 'nvim-lua/plenary.nvim'
+Plug 'nvim-telescope/telescope.nvim'
 vim.call('plug#end')
 
 require("nvim-tree").setup()
@@ -31,7 +33,7 @@ require'nvim-treesitter.configs'.setup {
 -- keymap('n', '<leader>t', 'NvimTreeToggle<cr>')
 vim.keymap.set('n', '<Leader>t', ':NvimTreeToggle<CR>')
 vim.keymap.set('n', '<Leader>f', ':NvimTreeFocus<CR>')
--- Functional wrapper for mapping custom keybindings
+-- Functional wrapper for mapping hjkl for Nvim tree
 function map(mode, lhs, rhs, opts)
     local options = { noremap = true }
     if opts then
@@ -40,3 +42,73 @@ function map(mode, lhs, rhs, opts)
     vim.api.nvim_set_keymap(mode, lhs, rhs, options)
 end
 vim.cmd[[colorscheme nord]]
+local lib = require("nvim-tree.lib")
+local view = require("nvim-tree.view")
+
+
+local function collapse_all()
+    require("nvim-tree.actions.tree-modifiers.collapse-all").fn()
+end
+
+local function edit_or_open()
+    -- open as vsplit on current node
+    local action = "edit"
+    local node = lib.get_node_at_cursor()
+
+    -- Just copy what's done normally with vsplit
+    if node.link_to and not node.nodes then
+        require('nvim-tree.actions.node.open-file').fn(action, node.link_to)
+        view.close() -- Close the tree if file was opened
+
+    elseif node.nodes ~= nil then
+        lib.expand_or_collapse(node)
+
+    else
+        require('nvim-tree.actions.node.open-file').fn(action, node.absolute_path)
+        view.close() -- Close the tree if file was opened
+    end
+
+end
+
+local function vsplit_preview()
+    -- open as vsplit on current node
+    local action = "vsplit"
+    local node = lib.get_node_at_cursor()
+
+    -- Just copy what's done normally with vsplit
+    if node.link_to and not node.nodes then
+        require('nvim-tree.actions.node.open-file').fn(action, node.link_to)
+
+    elseif node.nodes ~= nil then
+        lib.expand_or_collapse(node)
+
+    else
+        require('nvim-tree.actions.node.open-file').fn(action, node.absolute_path)
+
+    end
+
+    -- Finally refocus on tree if it was lost
+    view.focus()
+end
+local config = {
+    view = {
+        mappings = {
+            custom_only = false,
+            list = {
+                { key = "l", action = "edit", action_cb = edit_or_open },
+                { key = "L", action = "vsplit_preview", action_cb = vsplit_preview },
+                { key = "h", action = "close_node" },
+                { key = "H", action = "collapse_all", action_cb = collapse_all }
+            }
+        },
+    },
+    actions = {
+        open_file = {
+            quit_on_open = false
+        }
+    }
+}
+
+vim.api.nvim_set_keymap("n", "<C-h>", ":NvimTreeToggle<cr>" ,{silent = true, noremap = true})
+require('nvim-tree').setup(config)
+
